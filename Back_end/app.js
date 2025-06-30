105abc3c0aa709ddca11339ca13f7bdcf144204a
Meet Parmar <meetparmar2362004@gmail.com>
2025-06-30 08:55:17 +0530

    frontend changes

--- Back_end/app.js
+++ Back_end/app.js
@@ -19,7 +19,7 @@ app.set("trust proxy", 1);
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
   credentials: true
 }))
 app.use(express.json());
